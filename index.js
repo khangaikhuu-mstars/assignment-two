d1f6dc85d1114e42e2a29572c47cf9679b554d29
khangaikhuu uvgunkhuu <khangaikhuu@mstars.mn>
2021-12-01 16:38:53 +0800

    [REF] handle empty and wrong email

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ var server = http.createServer(app);
 app.use(express.json());
 app.use(cors());
 
+const errorMessage = {
+  status: "error",
+  message: "You have submitted empty email",
+};
+
 const createSql =
   "CREATE TABLE IF NOT EXISTS booking (id INTEGER PRIMARY KEY AUTOINCREMENT, email VARCHAR(255) NOT NULL,is_subscribed INTEGER NOT NULL DEFAULT 0 CHECK(is_subscribed IN (0,1)) );";
 
@@ -22,7 +27,7 @@ app.get("/subscribe/subscriptions", (req, res) => {
   const sql = "SELECT * FROM booking ORDER BY id";
   db.all(sql, [], (err, rows) => {
     if (err) {
-      return console.error(err.message);
+      return res.status(500).send(errorMessage);
     }
     res.json({ model: rows });
   });
@@ -34,7 +39,13 @@ app.post("/subscribe/subscription/create", (req, res) => {
   console.log(req.body);
   console.log(req.body["email"]);
   const book = [req.body.email, 1];
+  if (isEmpty(req.body)) {
+    return res.status(500).send(errorMessage);
+  }
   db.run(sql, book, (err) => {
+    if (err) {
+      return res.json(errorMessage);
+    }
     res.json({
       status: "success",
       message: "You have successfully subscribed",
@@ -45,3 +56,10 @@ app.post("/subscribe/subscription/create", (req, res) => {
 server.listen(3000, function () {
   console.log("Server listening on port: 3000");
 });
+
+function isEmpty(obj) {
+  for (var key in obj) {
+    if (obj.hasOwnProperty(key)) return false;
+  }
+  return true;
+}
