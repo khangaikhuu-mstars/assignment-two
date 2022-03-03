82110b65286b9129adb4f7cd7e71b85328fe665f
Ubuntu <ubuntu@ip-172-31-23-3.ap-southeast-1.compute.internal>
2022-03-03 06:51:14 +0000

    [REF] changing the port for the application

--- index.js
+++ index.js
@@ -50,8 +50,8 @@ app.post("/subscribe/subscription/create", (req, res) => {
   });
 });
 
-server.listen(3000, function () {
-  console.log("Server listening on port: 3000");
+server.listen(4000, function () {
+  console.log("Server listening on port: 4000");
 });
 
 function isEmpty(obj) {
